f47ca61f0a26920a84476662bedfb949435ba2e6
BuildTools <unconfigured@null.spigotmc.org>
2022-05-12 21:45:20 -0700

    Initial commit

---
+++ project-6-initial-master/aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -0,0 +1,38 @@
+package com.example.aggregator.controller;
+
+import com.example.aggregator.model.Entry;
+import com.example.aggregator.service.AggregatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AggregatorController {
+
+    private AggregatorService service;
+
+    public AggregatorController(AggregatorService service) { this.service = service; }
+
+    @GetMapping("/getDefinitionFor/{word}")
+    public Entry getDefinitionFor(@PathVariable String word) {
+
+        return service.getDefinitionFor(word);
+    }
+
+    @GetMapping("/getWordsStartingWith/{chars}")
+    public List<Entry> getWordsStartingWith(@PathVariable String chars) {
+
+        return service.getWordsStartingWith(chars);
+    }
+
+
+    @GetMapping("/getAllPalindromes/{word}")
+    public List<Entry> getAllPalindromes(@PathVariable String word) {
+
+        return service.getAllPalindromes();
+    }
+}
